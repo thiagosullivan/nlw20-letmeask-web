daea4e5658e26578aa801d4ed7cd5f47980db538
Thiago de Moura Santos <thiagosullivan12@gmail.com>
2025-10-03 03:40:36 -0300

    feat: slice the audio in small parts of transcriptions

--- src/pages/record-room-audio.tsx
+++ src/pages/record-room-audio.tsx
@@ -16,12 +16,17 @@ export function RecordRoomAudio() {
 
     const [isRecording, setIsRecording] = useState(false);
     const recorder = useRef<MediaRecorder | null>(null);
+    const intervalRef = useRef<NodeJS.Timeout>(null)
 
     function stopRecording() {
         setIsRecording(false);
 
         if(recorder.current && recorder.current.state !== 'inactive') {
             recorder.current.stop();
+        }
+
+        if(intervalRef.current){
+            clearInterval(intervalRef.current)
         }
 
     }
@@ -42,23 +47,7 @@ export function RecordRoomAudio() {
         return result;
     }
 
-    async function startRecording() {
-
-        if (!isRecordingSupported) {
-            alert('O seu navegador não suporta a gravação de áudio');
-            return;
-        }
-
-        setIsRecording(true);
-
-        const audio = await navigator.mediaDevices.getUserMedia({
-            audio: {
-                echoCancellation: true,
-                noiseSuppression: true,
-                sampleRate: 44_100,
-            },
-        });
-
+    function createRecorder(audio: MediaStream){
         recorder.current = new MediaRecorder(audio, {
             mimeType: 'audio/webm',
             audioBitsPerSecond: 64_000,
@@ -79,6 +68,32 @@ export function RecordRoomAudio() {
         }
 
         recorder.current.start();
+    }
+
+    async function startRecording() {
+
+        if (!isRecordingSupported) {
+            alert('O seu navegador não suporta a gravação de áudio');
+            return;
+        }
+
+        setIsRecording(true);
+
+        const audio = await navigator.mediaDevices.getUserMedia({
+            audio: {
+                echoCancellation: true,
+                noiseSuppression: true,
+                sampleRate: 44_100,
+            },
+        });
+
+        createRecorder(audio);
+
+        intervalRef.current = setInterval(() => {
+            recorder.current?.stop()
+
+            createRecorder(audio);
+        }, 5000)
     }
 
     if(!params.roomId){
